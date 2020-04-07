898b2234ca0c1536016dc802901a811d74318807
Keartroth <mecarrolljr@gmail.com>
2020-04-07 18:27:15 -0500

    added a nurseries directory and a nurseriesProvider.js; added nurseries to the database.json, and added foreign keys to the distrobution objects in database.json.

--- scripts/retailer/RetailerList.js
+++ scripts/retailer/RetailerList.js
@@ -1,6 +1,7 @@
 import { useRetailers } from "./retailerProvider.js";
 import { Retailer } from "./Retailer.js";
 import { useDistributors } from "../distributors/distributorsProvider.js";
+import { useFlowers } from "../flower/flowerProvider.js";
 
 const contentTarget = document.querySelector("#retailersList");
 const headerContentTarget = document.querySelector("#retailersHeader");
@@ -12,8 +13,12 @@ const render = (retailer, distributor) => {
 export const RetailerList = () => {
     const arrayOfRetailerObjects = useRetailers();
     const arrayOfDistributorObjects = useDistributors();
-    headerContentTarget.innerHTML = `<h2 class="bold">List of Local Flower Retailers</h2>`
+    const arrayOfFlowers = useFlowers()
+    headerContentTarget.innerHTML = `<h2 class="bold">Local Flower Retailers</h2>`
     arrayOfRetailerObjects.map(retailerObject => {
         const foundDistributorObject = arrayOfDistributorObjects.find(distro => distro.id === retailerObject.distributorsID)
+        const foundFlowers = arrayOfDistributorObjects.filter(distro => {
+            // THIS IS WHERE I AM
+        })
         render(retailerObject, foundDistributorObject)})
 }
