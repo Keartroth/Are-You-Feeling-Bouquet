cc4e2e41883a515572a6685e2e9aaf23205c82ca
Keartroth <mecarrolljr@gmail.com>
2020-04-07 17:09:28 -0500

    added distributors to the database.json; added a module to fetch the data; added the matching distributor to the flower card in Flowers.js.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
 import { getFlowers, useFlowers } from "./flower/flowerProvider.js";
 import { FlowerList } from "./flower/FlowerList.js";
-import { getRetailers, useRetailers } from "./retailer/retailerProvider.js";
+import { getRetailers } from "./retailer/retailerProvider.js";
 import { RetailerList } from "./retailer/RetailerList.js";
+import { getDistributors } from "./distributors/distributorsProvider.js";
 
 getFlowers()
     .then(useFlowers)
     .then(FlowerList)
 
 getRetailers()
-    .then(useRetailers)
+    .then(getDistributors)
     .then(RetailerList)
--- scripts/retailer/Retailer.js
+++ scripts/retailer/Retailer.js
@@ -1,12 +1,15 @@
-export const Retailer = (retailer) => {
+export const Retailer = (retailer, distributor) => {
     return `
         <section class="retailer">
             <header>
                 <h2>${retailer.name}</h2>
             </header>
             <div>
-                <p>Address: ${retailer.address}<br>
+                <p>Address: ${retailer.address}
+                <br>
                 ${retailer.city}, ${retailer.state}
+                <br><br>
+                Distributor: ${distributor.name}
                 </p>
             </div>
         </section>`
